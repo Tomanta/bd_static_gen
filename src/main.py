a4bcbb822596422323cc6defe1fb430f24ea4cf9
Zed <bzednick@gmail.com>
2024-12-23 17:22:40 -0600

    Can update public folder from static

--- src/main.py
+++ src/main.py
@@ -1,6 +1,26 @@
 from textnode import TextNode, TextType
 from htmlnode import LeafNode
 
+import os
+import shutil
+
+def clean_folder_if_exists(path):
+    if os.path.exists(path):
+        print(f"DEBUG: Removing files from {path}")
+        shutil.rmtree(path)
+
+def copy_folder(source, destination):
+    if not os.path.exists(source):
+        raise Exception(f"DEBUG: Source path does not exist! {source}, {destination}")
+    
+    clean_folder_if_exists(destination)
+    os.mkdir(destination)
+    for item in os.listdir(source):
+        if os.path.isfile(os.path.join(source, item)):
+            shutil.copy(os.path.join(source, item), destination)
+        else:
+            copy_folder(os.path.join(source, item), os.path.join(destination, item))
+
 def text_node_to_html_node(text_node: TextNode):
     match text_node.text_type:
         case TextType.TEXT:
@@ -19,8 +39,9 @@ def text_node_to_html_node(text_node: TextNode):
             raise ValueError("Invalid TextType")
 
 def main():
-    text_node = TextNode("This is a text node", TextType.BOLD, "http://www.boot.dev")
-    print(text_node)
+    static_root = './static'
+    public_root = './public'
+    copy_folder(static_root, public_root)
 
 if __name__ == "__main__":
     main()
